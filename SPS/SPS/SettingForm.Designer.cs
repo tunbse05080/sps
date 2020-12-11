eb198e4ba557f65ac57f897c31f2b89f86cb471a
CuongLe <hanovo.bom@gmail.com>
2020-12-11 15:19:38 +0700

    smart parking
    edit setting

--- SPS/SPS/SettingForm.Designer.cs
+++ SPS/SPS/SettingForm.Designer.cs
@@ -35,6 +35,8 @@
             this.rbtOut = new System.Windows.Forms.RadioButton();
             this.btnSave = new DevComponents.DotNetBar.ButtonX();
             this.btnCancel = new DevComponents.DotNetBar.ButtonX();
+            this.switchButton1 = new DevComponents.DotNetBar.Controls.SwitchButton();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
             this.SuspendLayout();
             // 
             // labelX1
@@ -44,7 +46,7 @@
             // 
             this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
             this.labelX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelX1.Location = new System.Drawing.Point(12, 12);
+            this.labelX1.Location = new System.Drawing.Point(12, 16);
             this.labelX1.Name = "labelX1";
             this.labelX1.Size = new System.Drawing.Size(141, 37);
             this.labelX1.TabIndex = 0;
@@ -57,7 +59,7 @@
             // 
             this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
             this.labelX2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelX2.Location = new System.Drawing.Point(12, 69);
+            this.labelX2.Location = new System.Drawing.Point(12, 151);
             this.labelX2.Name = "labelX2";
             this.labelX2.Size = new System.Drawing.Size(141, 37);
             this.labelX2.TabIndex = 1;
@@ -70,7 +72,7 @@
             this.comboBoxEx1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBoxEx1.FormattingEnabled = true;
             this.comboBoxEx1.ItemHeight = 33;
-            this.comboBoxEx1.Location = new System.Drawing.Point(159, 67);
+            this.comboBoxEx1.Location = new System.Drawing.Point(159, 149);
             this.comboBoxEx1.Name = "comboBoxEx1";
             this.comboBoxEx1.Size = new System.Drawing.Size(279, 39);
             this.comboBoxEx1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
@@ -80,10 +82,10 @@
             // 
             this.rbtIn.AutoSize = true;
             this.rbtIn.Checked = true;
-            this.rbtIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtIn.Location = new System.Drawing.Point(159, 16);
+            this.rbtIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtIn.Location = new System.Drawing.Point(159, 19);
             this.rbtIn.Name = "rbtIn";
-            this.rbtIn.Size = new System.Drawing.Size(133, 33);
+            this.rbtIn.Size = new System.Drawing.Size(140, 34);
             this.rbtIn.TabIndex = 3;
             this.rbtIn.TabStop = true;
             this.rbtIn.Text = "Cổng vào";
@@ -92,10 +94,10 @@
             // rbtOut
             // 
             this.rbtOut.AutoSize = true;
-            this.rbtOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtOut.Location = new System.Drawing.Point(322, 16);
+            this.rbtOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtOut.Location = new System.Drawing.Point(316, 19);
             this.rbtOut.Name = "rbtOut";
-            this.rbtOut.Size = new System.Drawing.Size(116, 33);
+            this.rbtOut.Size = new System.Drawing.Size(122, 34);
             this.rbtOut.TabIndex = 4;
             this.rbtOut.Text = "Cổng ra";
             this.rbtOut.UseVisualStyleBackColor = true;
@@ -105,7 +107,7 @@
             this.btnSave.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
             this.btnSave.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(159, 112);
+            this.btnSave.Location = new System.Drawing.Point(159, 212);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 37);
             this.btnSave.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
@@ -118,18 +120,49 @@
             this.btnCancel.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
             this.btnCancel.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(257, 112);
+            this.btnCancel.Location = new System.Drawing.Point(260, 212);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(78, 37);
             this.btnCancel.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "Hủy";
             // 
+            // switchButton1
+            // 
+            // 
+            // 
+            // 
+            this.switchButton1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.switchButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.switchButton1.Location = new System.Drawing.Point(322, 84);
+            this.switchButton1.Name = "switchButton1";
+            this.switchButton1.OffText = "Tắt";
+            this.switchButton1.OnText = "Bật";
+            this.switchButton1.Size = new System.Drawing.Size(94, 37);
+            this.switchButton1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.switchButton1.SwitchWidth = 48;
+            this.switchButton1.TabIndex = 9;
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX3.Location = new System.Drawing.Point(12, 84);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(287, 37);
+            this.labelX3.TabIndex = 10;
+            this.labelX3.Text = "Nhập biển số thủ công";
+            // 
             // SettingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 161);
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.switchButton1);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.rbtOut);
@@ -154,5 +187,7 @@
         private System.Windows.Forms.RadioButton rbtOut;
         private DevComponents.DotNetBar.ButtonX btnSave;
         private DevComponents.DotNetBar.ButtonX btnCancel;
+        private DevComponents.DotNetBar.Controls.SwitchButton switchButton1;
+        private DevComponents.DotNetBar.LabelX labelX3;
     }
 }
